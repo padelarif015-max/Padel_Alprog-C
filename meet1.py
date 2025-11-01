b0e6ded2ce6b641dbd84d5bad7a432188915fd32
muhammadfadelarif <padelarif015@gmail.com>
2025-11-01 23:49:21 +0800

    Update meet1.py

--- meet1.py
+++ meet1.py
@@ -1,4 +1,4 @@
-print("Nama saya amrina rosyada")
-print("Saya berasal dari lombok timur")
-print("umur saya 18 tahun")
-print("Hoby saya masak")
+print("Nama saya muhammad fadel arif")
+print("Saya berasal dari sumbawa")
+print("umur saya 19 tahun")
+print("Hoby saya main game")
