a5d46879c676626b3bfb20cc824e02085fd16128
muhammadfadelarif <padelarif015@gmail.com>
2025-11-15 20:50:16 +0800

    Update tugas algoritma.py

--- tugas algoritma.py
+++ tugas algoritma.py
@@ -1,12 +1,13 @@
 # tugas : buatlah program dengan output sebagai berikut :
 
-nama = input("masukan nama anda")
+nama = input("muhammad fadel arif")
 print("nama saya adalah ", nama)
 print(type(nama))
 
-umur = input(" masukan umur anda")
+umur = input("19")
 print("umur saya adalah", umur)
 print(type(umur))
+
 
 
 
